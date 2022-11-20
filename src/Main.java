ab1a19d4b5981412ab50f53d89defce0b77fc083
itsOtto <luhatuman@yandex.ru>
2022-11-20 14:25:26 +0900

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,20 @@ public class Main {
         } else if (clientOS2 == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
+
+        //Task 3
+        System.out.println();
+        System.out.println("Task 3");
+        short year = 2100;
+        if (year % 400 == 0 && year % 100 == 0) {
+            System.out.println(year + " - високосный год");
+        } else if (year % 4 == 0 && year % 100 > 0) {
+            System.out.println(year + " - високосный год");
+            } else if (year % 100 == 0) {
+            System.out.println(year + " - не високосный год");
+        } else {
+            System.out.println(year + " - високосный год");
+        }
+
     }
 }
