dd861c81af239e59393344fe7f2732f6038df4f1
itsOtto <luhatuman@yandex.ru>
2022-11-20 13:29:46 +0900

    Task 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //Task 1
+        System.out.println("Task 1");
+        byte clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+}
