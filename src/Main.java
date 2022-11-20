2cd79e2b59197119e856d2ffc29c79e572549ec4
itsOtto <luhatuman@yandex.ru>
2022-11-20 13:45:06 +0900

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,21 @@ public class Main {
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+
+        //Task 2
+        System.out.println();
+        System.out.println("Task 2");
+        short clientOS2 = 1; //0 - iOS, 1 - Android
+        short clientDeviceYear = 2014;
+        if (clientOS2 == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS2 == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS2 == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS2 == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
     }
 }
